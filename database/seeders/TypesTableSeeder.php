08e330888711bec540af686c34390dfc0fb71f88
MicheleFumi <michele.fumi96@gmail.com>
2025-03-11 10:17:48 +0100

    add type table and seeds

---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = ['Internet', 'Videogiochi', 'Web Design', 'Musica', 'Cinema'];
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+        }
+    }
+}
